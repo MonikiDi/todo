c2eb01fa8b1076efdba45618c39b273b624e5ca9
MonikiDi <d1e9n9i2s@gmail.com>
2023-09-18 22:45:54 +0300

    refactor (main) refactor render step 1

--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,6 @@ const spanDoneTask = document.querySelector('.done__value');
 const spanDoneTaskTwo = document.querySelector('.done__total');
 const explanations = document.querySelector('.explanations');
 
-
 function Task(text, status) {
 	this.id = Date.now();
 	this.text = text;
@@ -53,122 +52,79 @@ function TaskService() {
 	}
 }
 
-const taskService = new TaskService()
 
 
 
-const arrayTask = [];
 
 
-function creationObjectTask(text = todoInput.value, status = false) {
-	const task = new Task(text, status);
-	arrayTask.push(task);
-	return task;
+function template(task) {
+	return `
+		<li class="todo__item custom-checkbox__text">
+			<input id="done_task_${task.id}" type="checkbox" class="custom-checkbox__input" ${task.status && 'checked'}>
+			<p class="todo__p ${task.status && 'delete__p'}">${task.text}</p>
+			<button id="delete_${task.id}" class="btn-remove btn-reset"></button>
+		</li>
+	`
 }
 
-function render(where, element) {
-	where.append(element);
-	return element
-}
+let listener = [];
 
-function renderCheck() {
-	const check = document.createElement('input');
-	check.setAttribute('type', 'checkbox');
-	check.setAttribute('value', 'false');
-	check.classList.add('custom-checkbox__input');
-	return check
-}
+function renderTasks(taskService) {
+	const tasks = taskService.getAllTask();
 
-function renderP() {
-	const pTask = document.createElement('p');
-	pTask.classList.add('todo__p');
-	pTask.textContent = todoInput.value;
-	return pTask
-}
+	listener.forEach((unsubscribe) => unsubscribe());
+	listener = [];
 
-function renderBtnDelete() {
-	let removeBtn = document.createElement('button');
-	removeBtn.classList.add('btn-remove');
-	removeBtn.classList.add('btn-reset');
-	return removeBtn;
-}
+	todoList.innerHTML = '';
+	explanations.remove()
 
-function renderLi() {
-	let liTask = document.createElement('li');
-	liTask.classList.add('todo__item');
-	liTask.classList.add('custom-checkbox__text');
-	return liTask;
-}
+	spanValue.textContent = tasks.length.toString()
+	spanDoneTask.textContent = tasks.filter(({status}) => status).length.toString()
+	spanDoneTaskTwo.textContent = `из ${tasks.length}`;
 
-function deleteTask(removeBtn, liTask, id) {
-	removeBtn.addEventListener('click', () => {
-		const index = arrayTask.findIndex(n => n.id === id);
-		if (index !== -1) {
-			arrayTask.splice(index, 1);
-		}
-		liTask.remove();
-		showsTotalTasks();
-		showsCompletedTasks()
-		if (arrayTask.length == 0) {
-			render(todoTask, explanations)
-			spanDoneTaskTwo.textContent = '';
-		}
-		console.log(arrayTask);
+	if(!tasks.length) {
+		return todoTask.append(explanations)
+	}
+
+
+
+	todoList.innerHTML = tasks.map(template).join('')
+	tasks.forEach((task) => {
+		const nodeDelete = todoList.querySelector(`#delete_${task.id}`)
+		const nodeDone = todoList.querySelector(`#done_task_${task.id}`)
+
+		listener.push(addEvent(nodeDelete, 'click', () => {
+			taskService.removeTask(task.id);
+			renderTasks(taskService)
+		}))
+		listener.push(addEvent(nodeDone, 'click', (event) => {
+			taskService.updateTask(task.id, {
+				status: event.target.checked
+			});
+			renderTasks(taskService)
+		}))
 	})
 }
 
-function choiceCheck(check, task, pTask) {
-	check.addEventListener('click', (e) => {
-		let enabled = e.target.checked;
-		check.setAttribute('value', enabled);
-		task.status = enabled;
-		pTask.classList.remove('delete__p')
-		if (enabled) {
-			pTask.classList.add('delete__p');
-		}
-		showsCompletedTasks();
-		console.log(arrayTask);
-	})
+function addEvent(el, trigger, cb) {
+	el.addEventListener(trigger, cb)
+
+	return function removeEvent() {
+		el.removeEventListener(trigger,cb)
+	}
 }
 
-function showsTotalTasks() {
-	let totalTaks = arrayTask.length;
-	spanValue.textContent = totalTaks;
-}
+function app() {
+	const taskService = new TaskService();
 
-function showsCompletedTasks() {
-	let completedTasks = 0
-	arrayTask.forEach(e => {
-		if (e.status == true) {
-			completedTasks += 1
-		}
-	})
-	spanDoneTask.textContent = completedTasks;
-	spanDoneTaskTwo.textContent = `из ${arrayTask.length}`;
-}
-
-function renderTask() {
 	btnInput.addEventListener('click', () => {
-		if (todoInput.value) {
-			let task = creationObjectTask();
-			explanations.remove();
-			let liTask = renderLi();
-			let checkTask = render(liTask, renderCheck());
-			let pTask = render(liTask, renderP());
-			let removeBtn = render(liTask, renderBtnDelete());
-			render(todoList, liTask);
-			todoInput.value = '';
-			deleteTask(removeBtn, liTask, task.id);
-			choiceCheck(checkTask, task, pTask);
-			showsTotalTasks();
-			if (arrayTask.length !== 0) {
-				showsCompletedTasks();
-			}
-			console.log(arrayTask);
-		}
+
+		console.log(explanations)
+
+		taskService.addTask(todoInput.value);
+
+		renderTasks(taskService);
 	})
 }
 
-renderTask();
-
-
+app()
