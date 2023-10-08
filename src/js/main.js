1ddbac1db85b09da0c8e9dd9a5271307687edd70
Denis Grigorev <d1e9n9i2s@gmail.com>
2023-10-08 16:39:00 +0300

    added edit

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 const todoList = document.querySelector('.todo__list');
 const todoTask = document.querySelector('.todo__task');
+const todoHeader = document.querySelector('.todo__header');
 const todoCheck = document.querySelector('.todo__check');
-const todoInput = document.querySelector('.todo__input');
-const btnInput = document.querySelector('.todo__btn');
 const todoDone = document.querySelector('.todo__done');
 const spanValue = document.querySelector('.info__value');
 const doneContent = document.querySelector('.done__content');
@@ -48,7 +47,7 @@ function TaskService() {
 		removeTask: removeTask,
 		updateTask: updateTask,
 		getAllTask: getAllTask,
-		getTask: getTask
+		getTask: getTask,
 	}
 }
 
@@ -56,7 +55,7 @@ function addEvent(el, trigger, cb) {
 	el.addEventListener(trigger, cb)
 
 	return function removeEvent() {
-		el.removeEventListener(trigger,cb)
+		el.removeEventListener(trigger, cb)
 	}
 }
 
@@ -68,7 +67,8 @@ function TaskComponent(taskService) {
 		return `
 		<li class="todo__item custom-checkbox__text">
 			<input id="done_task_${task.id}" type="checkbox" class="custom-checkbox__input" ${task.status && 'checked'}>
-			<p class="todo__p ${task.status && 'delete__p'}">${task.text}</p>
+			<p id="text_${task.id}" class="todo__p ${task.status && 'delete__p'}">${task.text}</p>
+			<button id="edit_${task.id}" class="btn-edit btn-reset">Редактировать</button>
 			<button id="delete_${task.id}" class="btn-remove btn-reset"></button>
 		</li>
 	`
@@ -76,12 +76,11 @@ function TaskComponent(taskService) {
 
 	const renderTasks = () => {
 		eventUnsubscription();
-
 		clear();
 		renderInfoTask();
 
 		const tasks = this.taskService.getAllTask();
-		if(!tasks.length) {
+		if (!tasks.length) {
 			return todoTask.append(explanations)
 		}
 
@@ -95,14 +94,14 @@ function TaskComponent(taskService) {
 		explanations.remove()
 	}
 
-	const renderInfoTask =() => {
+	const renderInfoTask = () => {
 		const tasks = this.taskService.getAllTask();
 		spanValue.textContent = tasks.length.toString()
-		spanDoneTask.textContent = tasks.filter(({status}) => status).length.toString()
+		spanDoneTask.textContent = tasks.filter(({ status }) => status).length.toString()
 		spanDoneTaskTwo.textContent = `из ${tasks.length}`;
 	}
 
-	const  eventUnsubscription =() => {
+	const eventUnsubscription = () => {
 		this.listener.forEach((unsubscribe) => unsubscribe())
 		this.listener = [];
 	}
@@ -111,6 +110,8 @@ function TaskComponent(taskService) {
 		this.taskService.getAllTask().forEach((task) => {
 			const nodeDelete = todoList.querySelector(`#delete_${task.id}`)
 			const nodeDone = todoList.querySelector(`#done_task_${task.id}`)
+			const nodeEdit = todoList.querySelector(`#edit_${task.id}`)
+			const nodeP = todoList.querySelector(`#text_${task.id}`)
 
 			this.listener.push(addEvent(nodeDelete, 'click', () => {
 				this.taskService.removeTask(task.id);
@@ -121,7 +122,33 @@ function TaskComponent(taskService) {
 					status: event.target.checked
 				});
 				renderTasks(taskService)
+
 			}))
+			if(!(task.status == true)){
+			this.listener.push(addEvent(nodeEdit, 'click', () => {
+				InputEdit(nodeP, task)
+				nodeEdit.remove()
+
+				const nodeYes = todoList.querySelector(`#yes_${task.id}`)
+				const nodeNo = todoList.querySelector(`#no_${task.id}`)
+				const nodeInput = todoList.querySelector(`#input_${task.id}`)
+				nodeInput.value = task.text
+
+				this.listener.push(addEvent(nodeYes, 'click', () => {
+					const inputValue = nodeInput.value
+					if (!(inputValue == "")) {
+						taskService.updateTask(task.id, {
+							text: inputValue
+						});
+						renderTasks(taskService)
+					}
+				}))
+				this.listener.push(addEvent(nodeNo, 'click', () => {
+					renderTasks(taskService)
+				}))
+			}))} else {
+				nodeEdit.remove()
+			}
 		})
 	}
 
@@ -130,15 +157,45 @@ function TaskComponent(taskService) {
 	}
 }
 
+
+function InputComponent() {
+	function inputTask() {
+		return `
+		<input class="todo__input" type="text" placeholder="Добавить задачу">
+					<button class="todo__btn btn-reset">Создать</button>
+	`
+	}
+	return todoHeader.innerHTML = inputTask()
+}
+
+function InputEdit(nodeP, task) {
+	function inputEdit() {
+		return `
+		<input id="input_${task.id}" class="todo__input todo__input--edit" type="text" placeholder="Изменить задачу">
+		<button id="yes_${task.id}" class="todo__btn todo__btn--edit btn-reset">Подтвердить</button>
+		<button id="no_${task.id}" class="todo__btn todo__btn--edit btn-reset">Отменить</button>
+	`
+	}
+	return nodeP.innerHTML = inputEdit()
+}
+
+
 function app() {
+	InputComponent()
+	const btnInput = document.querySelector('.todo__btn');
+	const todoInput = document.querySelector('.todo__input');
+
 	const taskService = new TaskService();
 	const component = new TaskComponent(taskService);
-
 	component.render();
 
 	btnInput.addEventListener('click', () => {
-		taskService.addTask(todoInput.value)
-		component.render()
+		if (!(todoInput.value == "")) {
+			taskService.addTask(todoInput.value)
+			component.render()
+		}
+		todoInput.value = ""
+
 	})
 }
 
